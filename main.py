33813601cb88b4127af882040a9fc705ba96031f
rmsgh7000 <rmsgh70000@gmail.com>
2023-05-01 11:30:08 +0900

    t: Add items on lists (Trailing comma test)
    Let's play test

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 # Test python env
 def print_hello():
-    animals = ['dog','cat','hamster'] # in one line
+    animals = ['dog','cat','hamster', 'tiger'] # in one line
     foods = [
 	'pizza',
-	'spagetti',
+	'spagetti'
+	'bibimbob',
     ] # w/o trailing comma
     names = [
 	'John',
 	'Jane',
-	'Gil-dong',
+	'Gil-dong'
+	'Dong-eun',
     ] # w/ trailing comma
     for f_name in names:
 	print(f'hello, {f_name}')    
 
 if __name__ = '__main__':
-    print_hello()s
+    print_hello()
